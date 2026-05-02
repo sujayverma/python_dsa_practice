9703b18e9293cd3d2f64fc2aa85f511905cf0fbe
sujayverma <sujayverma124@gmail.com>
2026-05-02 13:17:47 +0530

    lindedlist solutions

---
+++ kth_node.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+  
+
+  
+ 
+  
+def find_kth_from_end(ll, k):       
+    
+    slow = ll.head
+    fast = ll.head
+    for _ in range(k):
+        if fast == None:
+            return None
+        fast = fast.next
+
+    while fast:
+        slow = slow.next
+        fast = fast.next
+
+    return slow
+
+    
+    
+
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+k = 1
+result = find_kth_from_end(my_linked_list, k)
+
+print(result.value)
+
+print(my_linked_list.head.value)
