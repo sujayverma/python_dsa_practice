1e0083042a2a00258ce0afc83dfaeb6e6b1f4ef6
sujayverma <sujayverma124@gmail.com>
2026-09-10 22:15:25 +0530

    Add Arrays

---
+++ practice/array.py
@@ -0,0 +1,15 @@
+# Traversal in Array. Traversal in array mean looping through an array.
+
+arr = [1,2,3,4,5]
+
+for i in arr:
+    print(i, end=' ')
+
+print()
+
+# Reverse Traversal of an Array
+
+for i in range(len(arr), 0, -1):
+    print(i, end=" ")
+
+print()
